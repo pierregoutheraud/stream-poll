41b7e7094469872caae6b8267e9b461cd6691343
Pierre Goutheraud <pierre.goutheraud@gmail.com>
2015-08-17 19:38:52 +0200

    react-router

--- app/scripts/App.jsx
+++ app/scripts/App.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import RVC from 'components/StreamPoll.jsx';
 
+import { Router, Route } from 'react-router';
+import { history } from 'react-router/lib/BrowserHistory';
+
 export default class App extends Component {
   render() {
     return (
-      <StreamPoll />
+      <Router history={history}>
+        <Route path="/" component={Home} >
+          <Route path="Home" component={Home} />
+          <Route path="/poll/:id" component={Poll} >
+            <Route path="/results" component={Results} />
+          </Route>
+        </Route>
+      </Router>
     );
   }
 }
