b0046444cb0d7150092d86a48ddb0633a3af72bd
Pierre Goutheraud <pierre.goutheraud@gmail.com>
2015-08-27 03:01:20 +0200

    Fix waiting status streampoll for viewer

--- app/scripts/pages/Widgets.jsx
+++ app/scripts/pages/Widgets.jsx
@@ -213,6 +213,16 @@ var Layout = React.createClass({
       return <Loading />
     };
 
+    let waiting = (
+      <div className="stream-poll__waiting">
+        <p>No poll created by the streamer yet.</p>
+        <p>Are you the streamer of this live ?</p>
+        <a href="" onClick={this.signin} className="twitch-signin" >
+          <img src='https://camo.githubusercontent.com/e3dadf5d1f371961805e6843fc7d9d611a1d14b5/687474703a2f2f7474762d6170692e73332e616d617a6f6e6177732e636f6d2f6173736574732f636f6e6e6563745f6461726b2e706e67'/>
+        </a>
+      </div>
+    );
+
     let widgetsFunctions = {
       'bigger':this.bigger,
       'smaller':this.smaller,
@@ -313,16 +323,6 @@ var Layout = React.createClass({
         </div>
       );
     }
-
-    let waiting = (
-      <div className="stream-poll__waiting">
-        <p>No poll created by the streamer yet.</p>
-        <p>Are you the streamer of this live ?</p>
-        <a href="" onClick={this.signin} className="twitch-signin" >
-          <img src='https://camo.githubusercontent.com/e3dadf5d1f371961805e6843fc7d9d611a1d14b5/687474703a2f2f7474762d6170692e73332e616d617a6f6e6177732e636f6d2f6173736574732f636f6e6e6563745f6461726b2e706e67'/>
-        </a>
-      </div>
-    );
 
     let stylePopoverLogin = {
       display: this.state.loginPopover ? 'block' : 'none'
