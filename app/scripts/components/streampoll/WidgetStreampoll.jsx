d0629d100e4274c34f9502af4f9827231fafec54
Pierre Goutheraud <pierre.goutheraud@gmail.com>
2015-09-08 19:29:58 +0200

    Text update

--- app/scripts/components/streampoll/WidgetStreampoll.jsx
+++ app/scripts/components/streampoll/WidgetStreampoll.jsx
@@ -177,7 +177,7 @@ var WidgetTwitchLive = React.createClass({
 
               <a href="" onClick={this.closePopover} className="popover__close" >skip</a>
 
-              <p>The streamer is not connected, are you {this.props.streamerUsername} ?</p>
+              <p>The streamer is currently not connected, are you {this.props.streamerUsername} ?</p>
               <a href="" onClick={this.signin} className="twitch-signin" >
                 <img src='https://camo.githubusercontent.com/e3dadf5d1f371961805e6843fc7d9d611a1d14b5/687474703a2f2f7474762d6170692e73332e616d617a6f6e6177732e636f6d2f6173736574732f636f6e6e6563745f6461726b2e706e67'/>
               </a>
